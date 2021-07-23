78a732a45032e0b426f9a6dfc5f433b34fd64005
Omran NAJJAR <omran.najjar@gmail.com>
2021-07-23 12:00:31 +0300

    key-stats for all regions

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -141,4 +141,66 @@ export class ApiController {
     }
   }
 
+  @get('/key-stats/{region}/{country}/{key}')
+  @response(200, {
+    description: 'region needs to be one of the following, asia, africa, central_america or south_america case sensitive',
+    content: { 'application/json': { schema: {} } },
+  })
+  async keyStats(
+    @param.path.string("region") region: string,
+    @param.path.string("country") country: string,
+    @param.path.string("key") key: string,
+
+  ) {
+
+    try {
+      return await this.queryRepository.execute(`with all_hot as (with acc_hot as (select  t.key, t.country , t.by_month, sum(total_count) total
+      from (select country, osm_changeset ,by_month  , "key" , total_count
+          from ${region}.keys_stats
+          where country = '${country}'
+          and "key" = '${key}') t
+      where t.osm_changeset in (select  id
+                    from public.hot_changeset )
+      group by  t.key, t.country  ,t.by_month)
+        select country,
+            key,
+            by_month,
+            total,
+            (select sum(total) from acc_hot inner_hot where  inner_hot.by_month <= acc.by_month) as accmulative_total
+        from acc_hot acc
+        order by 1, 3
+        ),
+      all_osm as ( with acc_osm as (select t.country, t.key , t.by_month, sum(total_count) total
+        from (select country, osm_changeset ,by_month  , "key" , total_count
+          from ${region}.keys_stats
+          where country = '${country}'
+          and "key" = '${key}') t
+        group by  t.key, t.country, t.by_month
+        order by 1, 3)
+        select country,
+            key,
+            by_month,
+            total,
+            (select sum(total) from acc_osm inner_osm where  inner_osm.by_month <= accO.by_month) as accmulative_total
+        from acc_osm accO
+        order by 1, 3)
+      select ah.country,
+      ah.key,
+      ah.by_month by_month_hot,
+      ao.by_month by_month_osm,
+      (case when ah.total is null then 0 else ah.total end) hot_mapped,
+      ah.accmulative_total  accmulative_total_hot,
+      ao.total all_osm_mapped,
+      ao.accmulative_total accmulative_total_osm
+      from all_hot ah full outer join all_osm ao
+      on ao.key = ah.key and ao.by_month = ah.by_month
+      order by 4 desc
+      `);
+    } catch (error) {
+
+      throw new Error(error);
+
+
+    }
+  }
 }
